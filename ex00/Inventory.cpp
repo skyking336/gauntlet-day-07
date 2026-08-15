08ee5dd5a6409e44ae6c1dd691e267c3d1e89451
skyking336 <3336jason@gmail.com>
2026-08-15 12:36:11 +0800

    ex00 test 2 pass

--- ex00/Inventory.cpp
+++ ex00/Inventory.cpp
@@ -10,6 +10,14 @@ int Inventory::count() const{
     return static_cast<int>(m_holdingItems.size());
 }
 
+bool Inventory::has(const std::string& item) const{
+    for(auto h = m_holdingItems.begin(); h != m_holdingItems.end();){
+        if (*h == item){
+            return true;
+        }
+    }
+    return false;
+}
 
 
 
